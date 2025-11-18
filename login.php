79cd95d61b2e4cc471eaff2125254284c54ae62c
MarceloC12 <101668685+MarceloC12@users.noreply.github.com>
2025-11-17 23:31:09 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciar Sesión</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <div class="container">
+
+        <div class="info">
+
+            <p class="txt-1"> Gracias por visitarnos</p>
+
+            <h2>Bienvenido de nuevo</h2>
+        </div>
+
+        <form class="form">
+            <h2>Iniciar Sesión</h2>
+            <div class="inputs">
+
+                <input type="email" class="box" placeholder="Ingresa tu correo">
+                <input type="password" class="box" placeholder="Ingresa tu contraseña">
+
+                <a href="#">Olvidaste tu contraseña</a>
+
+                <input type="submit" value="Ingresar">
+
+            </div>
+            <p>¿Todavia no tienes una cuenta?</p><a href="#">Registrate</a>
+        </form>
+
+
+    </div>
+
+</body>
+</html>
---
+++ registro.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <form>
+
+        <h2>Registro</h2>
+        <input type="text" placeholder="Nombre">
+        <input type="text" placeholder="Apellido">
+        <input type="email" placeholder="Correo">
+        <input type="password" placeholder="Contraseña">
+        <input type="password" placeholder="Confirmar Contraseña">
+        <input type="submit" name="btn" id="Enviar">
+    </form>
+    
+</body>
+</html>
